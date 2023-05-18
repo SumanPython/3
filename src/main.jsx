c769d60f7e2e696c463363ea91cf14a0b14ab5b8
SumanPython <spalisetty@gmail.com>
2023-05-18 13:47:00 +0530

    make changes to understand skipping a workflow [skip ci]

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+//making changes to get the understanding of skipping a workflow."
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
